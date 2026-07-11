e102afb8a570e641a65f8fbad1ed553074a0c442
lvzangrande <lvz@PC.localdomain>
2026-07-11 18:38:04 -0300

    Estilização e tentativa falha de esconder div ao aparecer outra div

--- ex010/script.js
+++ ex010/script.js
@@ -1,4 +1,4 @@
-
+//Criado sem usar condições if e else
 let nome = window.document.querySelector('input#nome');
 let email = window.document.querySelector('input#email');
 
@@ -9,33 +9,37 @@ let profi = window.document.querySelector('select#profissao');
 
 let data_nasc = window.document.querySelector('input#data_nasc');
 let hoje = new Date().getFullYear();
-let idade =  hoje - data_nasc;//para esse simples programa sem estrutura de condição farei desta forma
+let idade =  hoje - Number(data_nasc.value);//para esse simples programa sem estrutura de condição farei desta forma
 
 let max = window.document.querySelector('#data_nasc[max]');
-max = hoje;
+max = data_nasc.setAttribute('max',hoje)//função para dar valor para atributo;
 
-const botao = window.document.querySelector('input.botao');
+const botao_criar = window.document.querySelector('input.botao');
 
-/*dados = [
-    nome.value,
-    email.value,
-    tel.value,
-    profi.value,
-    idade.value    
-];*/
-console.log(botao);
-botao.addEventListener('click',criar);
+
+//let div_pergunta = window.document.querySelector('div.perguntas');
+botao_criar.addEventListener('click',criar);
 
 function criar(){ 
     alert('Perfil criado com sucesso!');
-    window.document.querySelector('div').style.display = 'none';
-    window.document.write(`
+    //window.document.querySelector('#1').style.display = 'none';
+    window.document.body.innerHTML += `
+        <div class='respostas' id='2'>
         <h1>SEUS DADOS:</h1>
         <p>Nome:<b>${nome.value}</b></p>
         <p>E-mail:<b>${email.value}</b></p>
         <p>Telefone:<b>${tel.value}</b></p>
-        <p>Idade:<b>${hoje - data_nasc.value} anos</b></p>
+        <p>Idade:<b>${idade} anos</b></p>
         <p>Profissao:<b>${profi.value}</b></p>
-        `);
-}
+        </div>`;
+        //<input type='button' id='del' value='Limpar'>
+        
+        
+        //window.document.querySelector('#del').addEventListener('click',limpar);
+}//ainda não aprendi a manter a estilização antes de executar o java script
+/*function limpar(){
+    window.alert('Dados limpos');
+    window.document.querySelector('#1').style.display = '';
+    window.document.querySelector('#2').remove();
+}não consegui fazer funcionar com meus conhecimentos atuais*/
 //addEventListener('')
